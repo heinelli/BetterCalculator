a898fac9d613754108eee076ec1ba6f40b080234
Elliot Heiner <elliotheiner1997@gmail.com>
2021-09-10 11:44:13 -0600

    Add first try catch statement.

--- BetterCalculator/BetterCalculator.vb
+++ BetterCalculator/BetterCalculator.vb
@@ -1,11 +1,20 @@
 ﻿Option Strict On
 Option Explicit On
 Module BetterCalculator
-
-
-
     Sub Main()
+        Dim num1 As Double
+
         Console.WriteLine("Please enter two numbers. Enter Q at any time to quit.")
+        Console.WriteLine("Choose a number")
+        Try
+            num1 = Double.Parse(Console.ReadLine())
+            Console.WriteLine(num1)
+            Console.Read()
+        Catch
+            Console.Write("Please enter a whole number.")
+            Console.Read()
+        End Try
+
 
     End Sub
 
