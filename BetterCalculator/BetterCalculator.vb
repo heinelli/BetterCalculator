f7de6ab12f768d6f758b203309d1306b51292710
Elliot Heiner <elliotheiner1997@gmail.com>
2021-09-10 14:39:53 -0600

    Add choice of operation and answer to operation.

--- BetterCalculator/BetterCalculator.vb
+++ BetterCalculator/BetterCalculator.vb
@@ -6,6 +6,8 @@ Module BetterCalculator
         Dim input1 As Boolean = False
         Dim num2 As Double
         Dim input2 As Boolean = False
+        Dim decision As Boolean = False
+        Dim operator1 As Integer
 
         Console.WriteLine("Please enter two numbers. Enter Q at any time to quit.")
         Console.WriteLine("Choose a number")
@@ -30,6 +32,37 @@ Module BetterCalculator
                 input2 = False
             End Try
         Loop
+        Console.WriteLine("Choose one of the following options:")
+        Console.WriteLine("1. Add")
+        Console.WriteLine("2. Subtract")
+        Console.WriteLine("3. Multiply")
+        Console.WriteLine("4. Divide")
+
+        Do While decision = False
+            Try
+                operator1 = CInt(Console.ReadLine())
+                If operator1 = 1 Then
+                    Console.WriteLine(num1 + num2)
+                ElseIf operator1 = 2 Then
+                    Console.WriteLine(num1 - num2)
+                ElseIf operator1 = 3 Then
+                    Console.WriteLine(num1 * num2)
+                ElseIf operator1 = 4 Then
+                    Console.WriteLine(num1 / num2)
+                End If
+                decision = True
+            Catch
+                Console.WriteLine("Choose one of the following options:")
+                Console.WriteLine("1. Add")
+                Console.WriteLine("2. Subtract")
+                Console.WriteLine("3. Multiply")
+                Console.WriteLine("4. Divide")
+                decision = False
+
+            End Try
+
+        Loop
+        Console.WriteLine("Good job!")
         Console.Read()
 
     End Sub
