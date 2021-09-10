0ede8bf7fba165a6b5858a6785b8f96ded00c25a
Elliot Heiner <elliotheiner1997@gmail.com>
2021-09-10 11:29:17 -0600

    Add project files.

---
+++ BetterCalculator/BetterCalculator.vb
@@ -0,0 +1,11 @@
+﻿Module BetterCalculator
+
+    Option Strict On
+    Option Explicit On
+
+    Sub Main()
+        Console.WriteLine("Please enter two numbers. Enter Q at any time to quit.")
+
+    End Sub
+
+End Module
