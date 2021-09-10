a2abf5ac0ed07290ec64191cf8d90e643c93b55c
Elliot Heiner <elliotheiner1997@gmail.com>
2021-09-10 14:23:50 -0600

    Add second loop with try catch statement.

--- BetterCalculator/BetterCalculator.vb
+++ BetterCalculator/BetterCalculator.vb
@@ -4,17 +4,30 @@ Module BetterCalculator
     Sub Main()
         Dim num1 As Double
         Dim input1 As Boolean = False
+        Dim num2 As Double
+        Dim input2 As Boolean = False
 
         Console.WriteLine("Please enter two numbers. Enter Q at any time to quit.")
         Console.WriteLine("Choose a number")
         Do While input1 = False
             Try
                 num1 = Double.Parse(Console.ReadLine())
-                Console.WriteLine(num1)
+                Console.WriteLine("You entered " & num1)
                 input1 = True
             Catch
-                Console.Write("Please enter a whole number.")
+                Console.WriteLine("Please enter a whole number.")
                 input1 = False
+            End Try
+        Loop
+        Console.WriteLine("Choose a second number.")
+        Do While input2 = False
+            Try
+                num2 = Double.Parse(Console.ReadLine())
+                Console.WriteLine("You entered " & num2)
+                input2 = True
+            Catch
+                Console.Write("Please enter a whole number.")
+                input2 = False
             End Try
         Loop
         Console.Read()
