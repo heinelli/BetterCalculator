ca5f85ed923943425857a53761ebb2686c51a698
Elliot Heiner <elliotheiner1997@gmail.com>
2021-09-10 12:06:38 -0600

    Add loop.

--- BetterCalculator/BetterCalculator.vb
+++ BetterCalculator/BetterCalculator.vb
@@ -3,18 +3,21 @@ Option Explicit On
 Module BetterCalculator
     Sub Main()
         Dim num1 As Double
+        Dim input1 As Boolean = False
 
         Console.WriteLine("Please enter two numbers. Enter Q at any time to quit.")
         Console.WriteLine("Choose a number")
-        Try
-            num1 = Double.Parse(Console.ReadLine())
-            Console.WriteLine(num1)
-            Console.Read()
-        Catch
-            Console.Write("Please enter a whole number.")
-            Console.Read()
-        End Try
-
+        Do While input1 = False
+            Try
+                num1 = Double.Parse(Console.ReadLine())
+                Console.WriteLine(num1)
+                input1 = True
+            Catch
+                Console.Write("Please enter a whole number.")
+                input1 = False
+            End Try
+        Loop
+        Console.Read()
 
     End Sub
 
