28e951159713ab08afe2e61432d8bdd157c223c4
Elliot Heiner <elliotheiner1997@gmail.com>
2021-09-11 20:47:26 -0600

    Add additional if then statements.

--- BetterCalculator/BetterCalculator.vb
+++ BetterCalculator/BetterCalculator.vb
@@ -13,104 +13,117 @@ Module BetterCalculator
         Dim exit1 As Boolean = False
 
 
-        Console.WriteLine("Please enter two numbers. Enter Q at any time to quit.")
-                Console.WriteLine("Choose a number")
-                Do While input1 = False
+        Do Until exit1 = True
+            Console.WriteLine("Please enter two numbers. Enter Q at any time to quit.")
+            Console.WriteLine("Choose a number")
+
+            Do While input1 = False
+                Try
+                    userInput = Console.ReadLine()
+                    num1 = CInt(userInput)
+                    Console.WriteLine("You entered " & num1)
+                    input1 = True
+                Catch
+                    If userInput = "Q" Then
+
+                        input1 = True
+                        exit1 = True
+
+                    Else
+                        Console.WriteLine("Please enter a whole number.")
+                        input1 = False
+                    End If
+
+
+                End Try
+            Loop
+            If exit1 <> True Then
+                Console.WriteLine("Choose a second number.")
+                Do While input2 = False
                     Try
                         userInput = Console.ReadLine()
-                        num1 = CInt(userInput)
-                        Console.WriteLine("You entered " & num1)
-                        input1 = True
+                        num2 = CInt(userInput)
+                        Console.WriteLine("You entered " & num2)
+                        input2 = True
                     Catch
                         If userInput = "Q" Then
 
-                            input1 = True
+                            input2 = True
                             exit1 = True
                         Else
                             Console.WriteLine("Please enter a whole number.")
-                            input1 = False
+                            input2 = False
                         End If
 
 
                     End Try
                 Loop
-                Console.WriteLine("Choose a second number.")
-        Do While input2 = False
-            Try
-                userInput = Console.ReadLine()
-                num2 = CInt(userInput)
-                Console.WriteLine("You entered " & num2)
-                input2 = True
-            Catch
-                If userInput = "Q" Then
+                If exit1 <> True Then
 
-                    input2 = True
-                    exit1 = True
-                Else
-                    Console.WriteLine("Please enter a whole number.")
+
+                    Console.WriteLine("Choose one of the following options:")
+                    Console.WriteLine("1. Add")
+                    Console.WriteLine("2. Subtract")
+                    Console.WriteLine("3. Multiply")
+                    Console.WriteLine("4. Divide")
+
+                    Do While decision = False
+                        Try
+                            userInput = (Console.ReadLine())
+                            operator1 = CInt(userInput)
+                            If operator1 = 1 Then
+                                Console.WriteLine("You entered 1")
+                                Console.WriteLine(num1 & "+" & num2 & "=" & num1 + num2)
+                                decision = True
+                            ElseIf operator1 = 2 Then
+                                Console.WriteLine("You entered 2")
+                                Console.WriteLine(num1 & "-" & num2 & "=" & num1 - num2)
+                                decision = True
+                            ElseIf operator1 = 3 Then
+                                Console.WriteLine("You entered 3")
+                                Console.WriteLine(num1 & "*" & num2 & "=" & num1 * num2)
+                                decision = True
+                            ElseIf operator1 = 4 Then
+                                Console.WriteLine("You entered 4")
+                                Console.WriteLine(num1 & "/" & num2 & "=" & num1 / num2)
+                                decision = True
+
+                            ElseIf operator1 > 4 Or operator1 < 1 Then
+                                decision = False
+                                Console.WriteLine("Choose one of the following options:")
+                                Console.WriteLine("1. Add")
+                                Console.WriteLine("2. Subtract")
+                                Console.WriteLine("3. Multiply")
+                                Console.WriteLine("4. Divide")
+                            End If
+
+                        Catch
+                            If userInput = "Q" Then
+                                decision = True
+                                exit1 = True
+                            Else
+                                Console.WriteLine("Choose one of the following options:")
+                                Console.WriteLine("1. Add")
+                                Console.WriteLine("2. Subtract")
+                                Console.WriteLine("3. Multiply")
+                                Console.WriteLine("4. Divide")
+                                decision = False
+
+                            End If
+                        End Try
+                    Loop
+                    input1 = False
                     input2 = False
+                    decision = False
                 End If
 
-
-            End Try
+            End If
         Loop
-        Console.WriteLine("Choose one of the following options:")
-                Console.WriteLine("1. Add")
-                Console.WriteLine("2. Subtract")
-                Console.WriteLine("3. Multiply")
-                Console.WriteLine("4. Divide")
 
-                Do While decision = False
-            Try
-                operator1 = CInt(Console.ReadLine())
-                If operator1 = 1 Then
-                    Console.WriteLine("You entered 1")
-                    Console.WriteLine(num1 & "+" & num2 & "=" & num1 + num2)
-                    decision = True
-                ElseIf operator1 = 2 Then
-                    Console.WriteLine("You entered 2")
-                    Console.WriteLine(num1 & "-" & num2 & "=" & num1 - num2)
-                    decision = True
-                ElseIf operator1 = 3 Then
-                    Console.WriteLine("You entered 3")
-                    Console.WriteLine(num1 & "*" & num2 & "=" & num1 * num2)
-                    decision = True
-                ElseIf operator1 = 4 Then
-                    Console.WriteLine("You entered 4")
-                    Console.WriteLine(num1 & "/" & num2 & "=" & num1 / num2)
-                    decision = True
-
-                ElseIf operator1 > 4 Or operator1 < 1 Then
-                    decision = False
-                    Console.WriteLine("Choose one of the following options:")
-                    Console.WriteLine("1. Add")
-                    Console.WriteLine("2. Subtract")
-                    Console.WriteLine("3. Multiply")
-                    Console.WriteLine("4. Divide")
-                End If
-
-            Catch
-                If userInput = "Q" Then
-                    decision = True
-                Else
-                    Console.WriteLine("Choose one of the following options:")
-                    Console.WriteLine("1. Add")
-                    Console.WriteLine("2. Subtract")
-                    Console.WriteLine("3. Multiply")
-                    Console.WriteLine("4. Divide")
-                    decision = False
-                End If
-            End Try
-                Loop
-                input1 = False
-                input2 = False
-                decision = False
-
-
-
-                Console.WriteLine("Good job!")
+        Console.WriteLine("Program terminated")
         Console.Read()
 
     End Sub
+
 
 End Module
